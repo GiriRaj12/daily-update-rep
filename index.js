2b125dd1c4e7792cc2947db0c9d33595bbda8cd4
Giri Raj <girirajsekar50@gmail.com>
2020-08-18 00:40:29 +0530

    Daily Updates

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 let json = require('jsonfile');
+const { default: simpleGit } = require('simple-git');
 
-let git = require('simple-git');
+let git = simpleGit();
 
 const PATH = "./date.json";
 
-const date = new Date();
+const DATE = new Date();
 
-let writeFile = {
-    Date : date,
-    Description: "Learning AWS hence cannot able to contribute in GIT"
+
+function commit_date(date){
+    let writeFile = {
+        Date : date,
+        Description: "Learning AWS hence cannot able to contribute in GIT"
+    }
+
+    json.writeFile(PATH,writeFile, () => {
+        console.log(date);
+        git.add([PATH])
+        git.commit("Daily Updates",[PATH],['--date',date])
+        git.push('origin', 'master');
+    });  
 }
 
-json.writeFile(PATH,writeFile);
+function makeForDays(){
+    for(let i=1;i<8;i++){
+        let date = new Date(DATE - (i * 86400000));
+        commit_date(date);
+    }
+}
 
 
-git().add(PATH).commit("Daily Updates"+date,{"--date":date}).push();
-
-
+makeForDays();
